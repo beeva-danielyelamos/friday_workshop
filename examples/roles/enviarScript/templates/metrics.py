ce2754d7eac6267fe01feb3a847e13797fa5cbd4
Carlos Medina <carlos.medina@beeva.com>
2016-11-11 12:16:46 +0100

    Modificado metrics.py para enviar datos a statd

--- examples/roles/enviarScript/templates/metrics.py
+++ examples/roles/enviarScript/templates/metrics.py
@@ -1,6 +1,12 @@
 import psutil
+from subprocess import call
 
 cpu_percent = psutil.cpu_percent()
 v_memory = psutil.virtual_memory().percent
 s_memory = psutil.swap_memory().percent
 d_usage = psutil.disk_usage('/').percent
+
+call(["echo 'cpu_percent:" + cpu_percent + "|c' | nc -u -w0 {{ ip_monitor }} {{ port_monitor }}"])
+call(["echo 'v_memory:" + v_memory + "|c' | nc -u -w0 {{ ip_monitor }} {{ port_monitor }}"])
+call(["echo 's_memory:" + s_memory + "|c' | nc -u -w0 {{ ip_monitor }} {{ port_monitor }}"])
+call(["echo 'd_usage:" + d_usage + "|c' | nc -u -w0 {{ ip_monitor }} {{ port_monitor }}"])
