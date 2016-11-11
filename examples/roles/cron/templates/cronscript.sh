852d5b62c96e7ac13669f75b373346bcf3bd4f29
Daniel Yelamos <daniel.yelamos@beeva.com>
2016-11-11 12:49:28 +0100

    fixed script varibale

--- examples/roles/cron/templates/cronscript.sh
+++ examples/roles/cron/templates/cronscript.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-PATH_TO_SCRIPT= {{ cron_script_path }}/{{cron_script_name}}
+PATH_TO_SCRIPT="{{ cron_script_path }}/metrics.py"
 
 for (( c=1; c<=6; c++ ))
 do  
    ${PATH_TO_SCRIPT}
-   wait(10)
+   sleep 10
 done
