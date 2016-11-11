b73e6b956298eaaf63db3467a3514c20deb19d42
Daniel Yelamos <daniel.yelamos@beeva.com>
2016-11-11 12:40:12 +0100

    modified crontscript

--- examples/roles/cron/templates/cronscript.sh
+++ examples/roles/cron/templates/cronscript.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-PATH_TO_SCRIPT= {{ cron_script_path }}/{cron_script_name}
+PATH_TO_SCRIPT= {{ cron_script_path }}/{{cron_script_name}}
 
 for (( c=1; c<=6; c++ ))
 do  
-   ./${PATH_TO_SCRIPT}
+   ${PATH_TO_SCRIPT}
    wait(10)
 done
